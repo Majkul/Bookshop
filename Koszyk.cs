9d2adcb5d863e29a010deb7937494c62b649b9c8
Korellek <72761623+Korellek@users.noreply.github.com>
2024-06-18 16:40:38 +0200

    Update Koszyk.cs

--- Koszyk.cs
+++ Koszyk.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class Koszyk{
+    private List<Produkt> listaProduktow;
+    private double wartosc;
+
+    public Koszyk(){
+        listaProduktow = new List<Produkt>();
+        wartosc = 0;
+    }
+
+    public List<Produkt> ListaProduktow{ get => listaProduktow; set => listaProduktow = value; }
+    public double Wartosc{ get => wartosc; set => wartosc = value; }
+}
