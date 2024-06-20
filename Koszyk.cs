c35232b760c96d696d4c53df2a255e56d615ab81
korellek <korelleks@gmail.com>
2024-06-20 21:52:28 +0200

    dokumentacja? more like dokumentacja? am i right? haha

--- Koszyk.cs
+++ Koszyk.cs
@@ -1,11 +1,25 @@
+/// <summary>
+/// Klasa reprezentująca produkt.
+/// </summary>
 class Koszyk{
     private List<Produkt> listaProduktow;
     private double wartosc;
 
+    /// <summary>
+    /// Konstruktor klasy Koszyk.
+    /// </summary>
     public Koszyk(){
         listaProduktow = new List<Produkt>();
         wartosc = 0;
     }
+
+    /// <summary>
+    /// Właściwość ListaProduktow, umożliwiająca dostęp do listy produktów w koszyku.
+    /// </summary>
     public List<Produkt> ListaProduktow{ get => listaProduktow; set => listaProduktow = value; }
+
+    /// <summary>
+    /// Właściwość Wartosc, umożliwiająca dostęp do wartości koszyka.
+    /// </summary>
     public double Wartosc{ get => wartosc; set => wartosc = value; }
 }
