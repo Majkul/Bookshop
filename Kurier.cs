675f0b4432749493020566b1dd82f534d8442b78
SoloBrainPixy <72761418+SoloBrainPixy@users.noreply.github.com>
2024-06-20 13:09:49 +0200

    exceptiony i dokumentacja

--- Kurier.cs
+++ Kurier.cs
@@ -6,12 +6,37 @@ class Kurier{
         }
     }
 
+
+
+    <summary>
+    Konstruktor
+    </summary>
+    <param name="zamowienie">Zamowienia, ktory kurier odbiera oraz dostarcza</param>
+    <author>Dominik</author>
     public Kurier(List<Zamowienie> zamowienia){
         this.zamowienia = zamowienia;
     }
 
+
+    <summary>
+    Sygnalizuje, ze kurier odbiera produkty z magazynu
+    Zmienia status zamowienia
+    </summary>
+    <author>Dominik</author>
     public void odbior(){
         // Zrealizowane odbiera
+        if(zamowienia.Count <= 0){
+           throw new Exception("Nie istnieja zamowienia do odebrania");
+        }
+        foreach(Zamowienie zamowienie in zamowienia){
+            if(zamowienie.ListaProduktow.Count <= 0){
+                throw new Exception("Zamowienie jest puste");
+            }
+            if(zamowienie.Status == "Zaplacone"){
+                zamowienie.ZmienStatus("W Drodze");
+            }
+        }
+
     }
 
     public void dostarcz(Zamowienie zamowienie){
--- Zamowienie.cs
+++ Zamowienie.cs
@@ -55,24 +55,102 @@ class Zamowienie{
             return this.wartosc;
         }
     }
+    private string link = "";
+    public string Link{
+        get{
+            return this.link;
+        }
+    }
+
+    private static string charget = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+
+
+    <summary>
+    Jedyny Konstruktor
+    </summary>
+    <param name="koszyk">Koszyk, z ktorego produkty sa zamawianie</param>
+    <param name="idKlienta">Id Klienta, do ktorego nalezy zamowienie</param>
+    <param name="Adres">Adres, gdize dostarczyc zamowienie</param>
+    <author>Dominik</author>
     public Zamowienie(Koszyk koszyk,int idKlienta,string adres){
+        
+        if(koszyk.ListaProduktow.Count <= 0){
+            throw new Exception("Koszyk jest pusty");
+        }
+        if(adres == "" || string.IsNullOrWhiteSpace(status)){
+            throw new Exception("Adres jest niepoprawy / nie istnieje");
+        }
+
+
         this.idKlienta = idKlienta;
         this.adres = adres;
         this.listaProduktow = koszyk.ListaProduktow;
-        Console.WriteLine(koszyk);
     }
-    public void kompletuj(Produkt produkt){
-        this.listaProduktow.Append<Produkt>(produkt);
+
+    <summary>
+    Kompletuje
+    </summary>
+    <param name="produkt">produkt</param>
+    <returns> Nic / bez returna </returns>
+    <author>Dominik</author>
+    //TODO zmienic to
+    //Czy to ma sens?
+    public void Kompletuj(Produkt produkt){
+        _ = this.listaProduktow.Append<Produkt>(produkt);
+        // _ = [...] discarduje returny
+        // pewnie mozna bez tego
     }
+
+
+    <summary>
+    Zmienia status zamowienia
+    </summary>
+    <param name="status">Nowy status zamowienia</param>
+    <returns>Nothing / No returns </returns>
+    <author>Dominik</author>
     public void zmienStatus(string status){
+        if(status = "" || string.IsNullOrWhiteSpace(status)){
+            throw new Exception("Nowy status jest pusty");
+        }
         this.status = status;
-        Console.WriteLine("Status updated to"+status);
+        
+        //debug
+        Console.WriteLine("[DEBUG] Status zostal zmieniony na: "+status);
     }
-    public string generujLink(int idKlienta,Produkt produkt){   //tutaj chyba można usunąć idKlienta bo ono się nie zmienia w trakcie życia zamówienia 
+
+
+
+    <summary>
+    Generuje link do cyfrowego produktu
+    </summary>
+    <returns>Link do cyfrowego produktu</returns>
+    <author>Dominik</author>
+    public string generujLink(){   
+        
+        if(link == ""){
+            string tmp = "https://www.CoolBookShop.com/pobierz/";
+            char[] chars = new char[16];
+            Random random = new Random();
+            
+            for(int i = 0,i < chars.Lenght, ++i ){
+                chars[i] = charget[random.Next(charget.Lenght)];
+            }
+
+            tmp+= new string(chars);
+            link += tmp;
+
+            tmp = null;
+            chars = null;
+            random = null;
+        }
+
+        return link;
+
         // Hiper zaawansowane generowanie linku za
         // pomoca sztucznej inteligencji CHATGPT
         // ORACLE COPILOT
-        return "hahha get scammed";
+        //return "hahha get scammed";
         // ayy Carumba
     }
 
